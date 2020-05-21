f045b301992362eb5570a8e50693e76d34d6ad55
Beachlessfawn2 <kenzie.coddens.y1678@student.hogent.be>
2020-05-21 11:13:58 +0200

    [Ini] ini repo

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import package.MessageUtil;
+
+public class Main{
+    public static void main(String[] args) {
+        MessageUtil mu = new MessageUtil("Idioten zijn er overal...");
+        mu.printMessage();
+        mu.salutationMessage();
+    }
+}
